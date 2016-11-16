a015059aca9fcc11fe2d932fc0badf61d54fa944
Oleg Slobodin <olegslobodin@mail.ru>
2016-11-16 23:46:35 +0300

    Linux build ok

--- Server/Header.h
+++ Server/Header.h
@@ -1,44 +1,59 @@
 #pragma once
 
-#include <Ws2tcpip.h>
 #include <stdio.h>
 #include <time.h>
 #include <iostream>
 #include <string>
-#include <winsock2.h>
 #include <string.h>
 #include <sstream>
 #include <vector>
 
-#pragma comment(lib, "Ws2_32.lib")
+#if  defined _WIN32 || defined _WIN64
+
+#include <Ws2tcpip.h>
+#include <winsock2.h>
 #define SocketPort 27015
-#define	 WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+
+#elif defined __linux__
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <malloc.h>
+#include <errno.h>
+#define SOCKET_ERROR -1
+#define SD_BOTH 2
+
+#endif
+
+#pragma comment(lib, "Ws2_32.lib")
 
 using namespace std;
 
 int ConfirmWinSocksDll();
 
-void InitServerSocket(int* serverSocket);
+void InitServerSocket(int *serverSocket);
 
 sockaddr_in GetServerSocketParams();
 
 void Work(int serverSocket);
 
-void ConnectClient(int serverSocket, vector<int>* clientSockets, vector<sockaddr_in>* clientSocketsParams);
+void ConnectClient(int serverSocket, vector<int> *clientSockets, vector<sockaddr_in> *clientSocketsParams);
 
-void CommandCycle(int clientIndex, vector<int>* clientSockets, vector<sockaddr_in>* clientSocketsParams);
+void CommandCycle(int clientIndex, vector<int> *clientSockets, vector<sockaddr_in> *clientSocketsParams);
 
-void InputCommand(char* recvBuffer, int clientSocket);
+void InputCommand(char *recvBuffer, int clientSocket);
 
-bool HasCommand(char* buffer);
+bool HasCommand(char *buffer);
 
-string TakeNextCommand(char* buffer);
+string TakeNextCommand(char *buffer);
 
-int ProceedCommand(string cmd, int clientIndex, vector<int>* clientSockets, vector<sockaddr_in>* clientSocketsParams);
+int ProceedCommand(string cmd, int clientIndex, vector<int> *clientSockets, vector<sockaddr_in> *clientSocketsParams);
 
-string CutLineEndings(char* cmd);
+string CutLineEndings(char *cmd);
 
-void CloseConnection(int clientIndex, vector<int>* clientSockets, vector<sockaddr_in>* clientSocketsParams);
+void CloseConnection(int clientIndex, vector<int> *clientSockets, vector<sockaddr_in> *clientSocketsParams);
 
 void Send(string str, int socket);
 
@@ -50,4 +65,4 @@ void split(const string &s, char delim, vector<string> &elems);
 
 vector<string> split(const string &s, char delim);
 
-vector<string> split(char* char_string, char delim);
+vector<string> split(char *char_string, char delim);
--- Server/Source.cpp
+++ Server/Source.cpp
@@ -3,275 +3,267 @@
 const int SHUTDOWN_SOCKET = 2;
 const int BUFFER_SIZE = 1000;
 
-int main()
-{
-	int serverSocket;
+int main() {
+    int serverSocket;
 
-	ConfirmWinSocksDll();
-	InitServerSocket(&serverSocket);
-	Work(serverSocket);
-	system("pause");
+#if  defined _WIN32 || defined _WIN64
+    ConfirmWinSocksDll();
+#endif
+
+    InitServerSocket(&serverSocket);
+    Work(serverSocket);
+
+#if  defined _WIN32 || defined _WIN64
+    system("pause");
+#endif
 }
 
-int ConfirmWinSocksDll()
-{
-	WORD wVersionRequested;
-	WSADATA wsaData;
-	int err;
-	wVersionRequested = MAKEWORD(2, 2);
+#if  defined _WIN32 || defined _WIN64
+int ConfirmWinSocksDll() {
+    WORD wVersionRequested;
+    WSADATA wsaData;
+    int err;
+    wVersionRequested = MAKEWORD(2, 2);
 
-	err = WSAStartup(wVersionRequested, &wsaData);
-	if (err != 0)
-	{
-		printf("ERROR: WSAStartup failed with error: %d\n", err);
-		return 1;
-	}
+    err = WSAStartup(wVersionRequested, &wsaData);
+    if (err != 0) {
+        printf("ERROR: WSAStartup failed with error: %d\n", err);
+        return 1;
+    }
 
-	if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
-	{
-		printf("ERROR: Could not find a usable version of Winsock.dll\n");
-		WSACleanup();
-		return 1;
-	}
+    if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2) {
+        printf("ERROR: Could not find a usable version of Winsock.dll\n");
+        WSACleanup();
+        return 1;
+    }
 
-	return 0;
+    return 0;
+}
+#endif
+
+void InitServerSocket(int *serverSocket) {
+    *serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (*serverSocket == SOCKET_ERROR) {
+        PrintLastError();
+        return;
+    }
+
+    sockaddr_in serverSocketParams = GetServerSocketParams();
+
+#if  defined _WIN32 || defined _WIN64
+    if (bind(*serverSocket, (LPSOCKADDR) & serverSocketParams, sizeof(serverSocketParams)) == SOCKET_ERROR) {
+        PrintLastError();
+        return;
+    }
+#elif defined __linux__
+    if (bind(*serverSocket, (sockaddr *) &serverSocketParams, sizeof(serverSocketParams)) == SOCKET_ERROR) {
+        PrintLastError();
+        return;
+    }
+#endif
+
+    if (listen(*serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+        PrintLastError();
+        return;
+    }
 }
 
-void InitServerSocket(int* serverSocket)
-{
-	*serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (*serverSocket == SOCKET_ERROR)
-	{
-		PrintLastError();
-		return;
-	}
+sockaddr_in GetServerSocketParams() {
+    sockaddr_in sin;
 
-	sockaddr_in serverSocketParams = GetServerSocketParams();
+    sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_port = htons(80);
+    sin.sin_family = AF_INET;
 
-	if (bind(*serverSocket, (LPSOCKADDR)&serverSocketParams, sizeof(serverSocketParams)) == SOCKET_ERROR)
-	{
-		PrintLastError();
-		return;
-	}
-
-	if (listen(*serverSocket, SOMAXCONN) == SOCKET_ERROR)
-	{
-		PrintLastError();
-		return;
-	}
+    return sin;
 }
 
-sockaddr_in GetServerSocketParams()
-{
-	sockaddr_in sin;
+void Work(int serverSocket) {
+    vector<int> *clientSockets = new vector<int>;
+    vector<sockaddr_in> *clientSocketsParams = new vector<sockaddr_in>;
 
-	sin.sin_addr.s_addr = INADDR_ANY;
-	sin.sin_port = htons(80);
-	sin.sin_family = AF_INET;
-
-	return sin;
+    while (true) {
+        ConnectClient(serverSocket, clientSockets, clientSocketsParams);
+        for (vector<int>::iterator clientSocket = clientSockets->begin();
+             clientSocket != clientSockets->end(); clientSocket++) {
+            int clientSocketIndex = clientSocket - clientSockets->begin();
+            CommandCycle(clientSocketIndex, clientSockets, clientSocketsParams);
+            if (clientSockets->size() == 0)
+                break;
+        }
+    }
 }
 
-void Work(int serverSocket)
-{
-	vector<int>* clientSockets = new vector<int>;
-	vector<sockaddr_in>* clientSocketsParams = new vector<sockaddr_in>;
+void ConnectClient(int serverSocket, vector<int> *clientSockets, vector<sockaddr_in> *clientSocketsParams) {
+    sockaddr_in clientSocketParams;
 
-	while (true)
-	{
-		ConnectClient(serverSocket, clientSockets, clientSocketsParams);
-		for (vector<int>::iterator clientSocket = clientSockets->begin(); clientSocket != clientSockets->end(); clientSocket++)
-		{
-			int clientSocketIndex = clientSocket - clientSockets->begin();
-			CommandCycle(clientSocketIndex, clientSockets, clientSocketsParams);
-			if (clientSockets->size() == 0)
-				break;
-		}
-	}
+#if  defined _WIN32 || defined _WIN64
+    sockaddr_in clientSocketParams;
+#elif defined __linux__
+    socklen_t fromlen = sizeof(clientSocketParams);
+#endif
+
+    int newClientSocket = accept(serverSocket, (struct sockaddr *) &clientSocketParams, &fromlen);
+    if (newClientSocket == SOCKET_ERROR) {
+        PrintLastError();
+        return;
+    }
+
+    clientSockets->push_back(newClientSocket);
+    clientSocketsParams->push_back(clientSocketParams);
+
+    Send("Connection established!", newClientSocket);
+    printf("accepted connection from %s, port %d\n", inet_ntoa(clientSocketParams.sin_addr),
+           htons(clientSocketParams.sin_port));
 }
 
-void ConnectClient(int serverSocket, vector<int>* clientSockets, vector<sockaddr_in>* clientSocketsParams)
-{
-	sockaddr_in clientSocketParams;
-	int fromlen = sizeof(clientSocketParams);
+void CommandCycle(int clientIndex, vector<int> *clientSockets, vector<sockaddr_in> *clientSocketsParams) {
+    char *buffer = (char *) calloc(BUFFER_SIZE, 1);
+    int currentClientSocket = (*clientSockets)[clientIndex];
+    bool breakCycle = false;
 
-	int newClientSocket = accept(serverSocket, (struct sockaddr*)&clientSocketParams, &fromlen);
-	if (newClientSocket == SOCKET_ERROR)
-	{
-		PrintLastError();
-		return;
-	}
-
-	clientSockets->push_back(newClientSocket);
-	clientSocketsParams->push_back(clientSocketParams);
-
-	Send("Connection established!", newClientSocket);
-	printf("accepted connection from %s, port %d\n", inet_ntoa(clientSocketParams.sin_addr), htons(clientSocketParams.sin_port));
+    while (!breakCycle) {
+        InputCommand(buffer, currentClientSocket);
+        while (HasCommand(buffer)) {
+            string cmd = TakeNextCommand(buffer);
+            cout << cmd << endl;
+            if (ProceedCommand(cmd, clientIndex, clientSockets, clientSocketsParams) != 0) {
+                breakCycle = true;
+                break;
+            }
+        }
+    }
+    free(buffer);
 }
 
-void CommandCycle(int clientIndex, vector<int>* clientSockets, vector<sockaddr_in>* clientSocketsParams)
-{
-	char* buffer = (char*)calloc(BUFFER_SIZE, 1);
-	int currentClientSocket = (*clientSockets)[clientIndex];
-	bool breakCycle = false;
+void InputCommand(char *buffer, int clientSocket) {
+    //find position of line ending
+    char *currentPos = strchr(buffer, '\0');
+    if (currentPos == NULL)
+        currentPos = buffer;
 
-	while (!breakCycle)
-	{
-		InputCommand(buffer, currentClientSocket);
-		while (HasCommand(buffer))
-		{
-			string cmd = TakeNextCommand(buffer);
-			cout << cmd << endl;
-			if (ProceedCommand(cmd, clientIndex, clientSockets, clientSocketsParams) != 0)
-			{
-				breakCycle = true;
-				break;
-			}
-		}
-	}
-	free(buffer);
+    int bytesRecieved = recv(clientSocket, currentPos, BUFFER_SIZE - (currentPos - buffer), 0);
+    if (bytesRecieved == SOCKET_ERROR) {
+        buffer[0] = '\0';
+        return;
+    }
+    currentPos += bytesRecieved;
+    *currentPos = '\0';
 }
 
-void InputCommand(char* buffer, int clientSocket)
-{
-	//find position of line ending
-	char* currentPos = strchr(buffer, '\0');
-	if (currentPos == NULL)
-		currentPos = buffer;
-
-	int bytesRecieved = recv(clientSocket, currentPos, BUFFER_SIZE - (currentPos - buffer), 0);
-	if (bytesRecieved == SOCKET_ERROR)
-	{
-		buffer[0] = '\0';
-		return;
-	}
-	currentPos += bytesRecieved;
-	*currentPos = '\0';
+bool HasCommand(char *buffer) {
+    return strchr(buffer, '\n') != NULL;
 }
 
-bool HasCommand(char* buffer)
-{
-	return strchr(buffer, '\n') != NULL;
+string TakeNextCommand(char *buffer) {
+    char *endLine = strchr(buffer, '\n');
+    int cmdLength = endLine - buffer;
+    string cmd;
+    cmd.assign(buffer, cmdLength);
+    for (int i = 0; i < BUFFER_SIZE - cmdLength + 1; i++)
+        buffer[i] = buffer[i + cmdLength + 1];
+    return cmd;
 }
 
-string TakeNextCommand(char* buffer)
-{
-	char* endLine = strchr(buffer, '\n');
-	int cmdLength = endLine - buffer;
-	string cmd;
-	cmd.assign(buffer, cmdLength);
-	for (int i = 0; i < BUFFER_SIZE - cmdLength + 1; i++)
-		buffer[i] = buffer[i + cmdLength + 1];
-	return cmd;
+int ProceedCommand(string cmd, int clientIndex, vector<int> *clientSockets, vector<sockaddr_in> *clientSocketsParams) {
+    vector<string> words = split(cmd, ' ');
+    int currentClientSocket = (*clientSockets)[clientIndex];
+
+    if (words.empty()) {
+        return 0;
+    }
+
+    if (words[0].compare("echo") == 0) {
+        if (cmd.length() > 5)
+            Send(cmd.substr(5), currentClientSocket);
+    } else if (words[0].compare("time") == 0)
+        Send(GetTime(), currentClientSocket);
+
+    else if (words[0].compare("close") == 0) {
+        CloseConnection(clientIndex, clientSockets, clientSocketsParams);
+        return SHUTDOWN_SOCKET;
+    } else {
+        Send("Command not found!", currentClientSocket);
+    }
+
+    return 0;
 }
 
-int ProceedCommand(string cmd, int clientIndex, vector<int>* clientSockets, vector<sockaddr_in>* clientSocketsParams)
-{
-	vector<string> words = split(cmd, ' ');
-	int currentClientSocket = (*clientSockets)[clientIndex];
-
-	if (words.empty())
-	{
-		return 0;
-	}
-
-	if (words[0].compare("echo") == 0)
-	{
-		if (cmd.length() > 5)
-			Send(cmd.substr(5), currentClientSocket);
-	}
-
-	else if (words[0].compare("time") == 0)
-		Send(GetTime(), currentClientSocket);
-
-	else if (words[0].compare("close") == 0)
-	{
-		CloseConnection(clientIndex, clientSockets, clientSocketsParams);
-		return SHUTDOWN_SOCKET;
-	}
-	else
-	{
-		Send("Command not found!", currentClientSocket);
-	}
-
-	return 0;
+string CutLineEndings(char *cmd) {
+    string line;
+    line.assign(cmd);
+    while (!line.empty() && (line[line.length() - 1] == '\n' || line[line.length() - 1] == '\r'))
+        line.erase(line.length() - 1);
+    return line;
 }
 
-string CutLineEndings(char* cmd)
-{
-	string line;
-	line.assign(cmd);
-	while (!line.empty() && (line[line.length() - 1] == '\n' || line[line.length() - 1] == '\r'))
-		line.erase(line.length() - 1);
-	return line;
+void CloseConnection(int clientIndex, vector<int> *clientSockets, vector<sockaddr_in> *clientSocketsParams) {
+    int currentClientSocket = (*clientSockets)[clientIndex];
+    sockaddr_in currentClientSocketParams = (*clientSocketsParams)[clientIndex];
+
+    Send("Good bye!", currentClientSocket);
+    printf("Connection closed with %s, port %d\n", inet_ntoa(currentClientSocketParams.sin_addr),
+           htons(currentClientSocketParams.sin_port));
+    shutdown(currentClientSocket, SD_BOTH);
+
+    clientSockets->erase(clientSockets->begin() + clientIndex);
+    clientSocketsParams->erase(clientSocketsParams->begin() + clientIndex);
 }
 
-void CloseConnection(int clientIndex, vector<int>* clientSockets, vector<sockaddr_in>* clientSocketsParams)
-{
-	int currentClientSocket = (*clientSockets)[clientIndex];
-	sockaddr_in currentClientSocketParams = (*clientSocketsParams)[clientIndex];
-
-	Send("Good bye!", currentClientSocket);
-	printf("Connection closed with %s, port %d\n", inet_ntoa(currentClientSocketParams.sin_addr), htons(currentClientSocketParams.sin_port));
-	shutdown(currentClientSocket, SD_BOTH);
-
-	clientSockets->erase(clientSockets->begin() + clientIndex);
-	clientSocketsParams->erase(clientSocketsParams->begin() + clientIndex);
+void Send(string str, int socket) {
+    int bufSize = str.length();
+    char *buf = (char *) calloc(bufSize + 1, 1);
+    strcpy(buf, str.c_str());
+    if (send(socket, buf, bufSize, MSG_DONTROUTE) == -1)
+        PrintLastError();
+    free(buf);
 }
 
-void Send(string str, int socket)
-{
-	int bufSize = str.length();
-	char* buf = (char*)calloc(bufSize + 1, 1);
-	strcpy(buf, str.c_str());
-	if (send(socket, buf, bufSize, MSG_DONTROUTE) == -1)
-		PrintLastError();
-	free(buf);
+string GetTime() {
+    time_t rawtime;
+    struct tm *timeinfo;
+    char buffer[80];
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+
+    strftime(buffer, 80, "%d-%m-%Y %I:%M:%S", timeinfo);
+    string str(buffer);
+
+    return str;
 }
 
-string GetTime()
-{
-	time_t rawtime;
-	struct tm * timeinfo;
-	char buffer[80];
-
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
-
-	strftime(buffer, 80, "%d-%m-%Y %I:%M:%S", timeinfo);
-	string str(buffer);
-
-	return str;
-}
-
-void PrintLastError()
-{
-	wchar_t buf[256];
-	FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(),
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), buf, 256, NULL);
-	wcout << buf;
+void PrintLastError() {
+#if  defined _WIN32 || defined _WIN64
+    wchar_t buf[256];
+    FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(),
+                   MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), buf, 256, NULL)
+    wcout << buf;
+#elif defined __linux__
+    cout << strerror(errno);
+#endif
 }
 
 void split(const string &s, char delim, vector<string> &elems) {
-	stringstream ss;
-	ss.str(s);
-	string item;
-	while (getline(ss, item, delim)) {
-		if (item.length())
-			elems.push_back(item);
-	}
+    stringstream ss;
+    ss.str(s);
+    string item;
+    while (getline(ss, item, delim)) {
+        if (item.length())
+            elems.push_back(item);
+    }
 }
 
 vector<string> split(const string &s, char delim) {
-	vector<string> elems;
-	split(s, delim, elems);
-	return elems;
+    vector<string> elems;
+    split(s, delim, elems);
+    return elems;
 }
 
-vector<string> split(char* char_string, char delim) {
-	string s;
-	s.assign(char_string);
-	vector<string> elems;
-	split(s, delim, elems);
-	return elems;
+vector<string> split(char *char_string, char delim) {
+    string s;
+    s.assign(char_string);
+    vector<string> elems;
+    split(s, delim, elems);
+    return elems;
 }
